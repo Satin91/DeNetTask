f129b6181edb07e081c751faffab641611f319e2
Satin91 <cool19912015@gmail.com>
2023-03-14 10:13:15 +0300

    Initial Commit

---
+++ DeNetTask/DeNetTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeNetTaskApp.swift
+//  DeNetTask
+//
+//  Created by Артур Кулик on 14.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct DeNetTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
